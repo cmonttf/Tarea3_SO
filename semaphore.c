a3e2e18b6b98f880f8a02ee5d049569508982508
Camilo Montt Fierro <55846057+cmonttf@users.noreply.github.com>
2022-07-28 21:05:25 -0400

    Update semaphore.c

--- semaphore.c
+++ semaphore.c
@@ -1,6 +1,6 @@
 
 /* Programa de ejemplo de semaphore. */
-/* para compilar usar: gcc -o tarea3_ejemplo tarea3_ejemplo.c -lpthread */
+/* para compilar usar: gcc -o semaphore semaphore.c -lpthread */
 
 #include <pthread.h>
 #include <semaphore.h>
